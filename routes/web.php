7d05ab7c7d106a1fab22da3eb9380012d51b116a
Header-Develop <kritawitbunket@gmail.com>
2017-04-25 15:39:40 +0700

    -config default route

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
 
-Route::get('admin', 'AdminController@index');
+Route::get('/', 'AdminController@index');
